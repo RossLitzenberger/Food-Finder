6d534670331e1a725cfdc2c7faf91a989cb02fc2
RossLitz <codingclasses.049@gmail.com>
2015-04-14 09:59:15 -0500

    fix the error

--- init.rb
+++ init.rb
@@ -3,6 +3,9 @@
 # Launch this Ruby file from the commend line
 # to get started.
 # 
-APP_ROOT = File.dirname(__FILE___)
+APP_ROOT = File.dirname(__FILE__)
 
-require File.join(APP_ROOT,'lib','guide')
+# require File.join(APP_ROOT,'lib','guide')
+
+$:.unshift(File.join(APP_ROOT,'lib') )
+require 'guide'
